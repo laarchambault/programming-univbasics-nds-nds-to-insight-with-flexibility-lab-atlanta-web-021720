1748a22a7bb6fbd574372a0821737f075995dffc
Laura Archambault <laarchambault@gmail.com>
2019-10-31 21:16:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,6 +75,7 @@ def movies_with_directors_set(source)
     name = source[index][:name]
     movies = source[index][:movies]
     new_array << movies_with_director_key(name, movies)
+    binding.pry
     index += 1
   end
   new_array
