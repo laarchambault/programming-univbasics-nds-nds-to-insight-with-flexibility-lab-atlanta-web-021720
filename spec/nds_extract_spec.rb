d87b1b1a08c3ba715aaa95a0e8892b04b267fc0b
Laura Archambault <laarchambault@gmail.com>
2019-10-31 21:03:57 +0000

    Automatically backed up by Learn

--- spec/nds_extract_spec.rb
+++ spec/nds_extract_spec.rb
@@ -1,74 +1,74 @@
 require 'spec_helper'
 
-# describe 'provided method' do
-#   describe 'flatten_a_o_a' do
-#     it 'flattens an a_a_a of Integers' do
-#       expect(flatten_a_o_a([[1,2],[3,4,5],[6]])).to eq([1,2,3,4,5,6])
-#     end
-#   end
+describe 'provided method' do
+  describe 'flatten_a_o_a' do
+    it 'flattens an a_a_a of Integers' do
+      expect(flatten_a_o_a([[1,2],[3,4,5],[6]])).to eq([1,2,3,4,5,6])
+    end
+  end
 
-#   describe 'movie_with_director_name' do
-#     it "creates a new movie Hash with director's name provided" do
-#       director_name = "Byron Poodle"
-#       movies_coll = {
-#         :worldwide_gross => 2,
-#         :release_year => 2014,
-#         :studio => "Karbit Poodles",
-#         :title => "The Fire Hydrant of Doom"
-#       }
-#       new_movie = movie_with_director_name(director_name, movies_coll)
+  describe 'movie_with_director_name' do
+    it "creates a new movie Hash with director's name provided" do
+      director_name = "Byron Poodle"
+      movies_coll = {
+        :worldwide_gross => 2,
+        :release_year => 2014,
+        :studio => "Karbit Poodles",
+        :title => "The Fire Hydrant of Doom"
+      }
+      new_movie = movie_with_director_name(director_name, movies_coll)
 
-#       expect(new_movie[:director_name]).to eq(director_name)
-#     end
-#   end
-# end
+      expect(new_movie[:director_name]).to eq(director_name)
+    end
+  end
+end
 
-# describe 'movies_with_director_key' do
-#   it 'adds a :director_name key an AoH of movies' do
-#     dir_name = "Byron Poodle"
-#     test_set = [{:title => "TestA"}, {:title => "TestB"}]
-#     updated_movies = movies_with_director_key(dir_name, test_set)
-#     expect(updated_movies[0][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
-#     expect(updated_movies[1][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
-#   end
-# end
+describe 'movies_with_director_key' do
+  it 'adds a :director_name key an AoH of movies' do
+    dir_name = "Byron Poodle"
+    test_set = [{:title => "TestA"}, {:title => "TestB"}]
+    updated_movies = movies_with_director_key(dir_name, test_set)
+    expect(updated_movies[0][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
+    expect(updated_movies[1][:director_name]).to eq(dir_name), "Should add director name to each movie Hash"
+  end
+end
 
-# describe 'movies_with_directors_set' do
-#   describe 'when given a Hash with keys :name and :movies,' do
-#     describe 'returns an Array of Hashes that represent movies' do
-#       describe 'and each Hash has a :director_name key set with the value that was in :name' do
-#         # This lets "sample_data" be used in the two "it" statements below
-#         let (:test_data) {
-#           [
-#             { :name => "Byron Poodle", :movies => [
-#               { :title => "At the park" },
-#               { :title => "On the couch" },
-#             ]
-#             },
-#             { :name => "Nancy Drew", :movies => [
-#               { :title => "Biting" },
-#             ]
-#             }
-#           ] 
-#         }
+describe 'movies_with_directors_set' do
+  describe 'when given a Hash with keys :name and :movies,' do
+    describe 'returns an Array of Hashes that represent movies' do
+      describe 'and each Hash has a :director_name key set with the value that was in :name' do
+        # This lets "sample_data" be used in the two "it" statements below
+        let (:test_data) {
+          [
+            { :name => "Byron Poodle", :movies => [
+              { :title => "At the park" },
+              { :title => "On the couch" },
+            ]
+            },
+            { :name => "Nancy Drew", :movies => [
+              { :title => "Biting" },
+            ]
+            }
+          ] 
+        }
 
-#         it 'correctly "distributes" Byron Poodle as :director_name of the first film' do
-#           # { :name => "A", :movies => [{ :title => "Test" }] }
-#           # becomes... [[{:title => "Test", :director_name => "A"}], ...[], ... []]
-#           results = movies_with_directors_set(test_data)
-#           expect(results.first.first[:director_name]).to eq("Byron Poodle"),
-#             "The first element of the AoA should have 'Byron Poodle' as :director_name"
-#         end
+        it 'correctly "distributes" Byron Poodle as :director_name of the first film' do
+          # { :name => "A", :movies => [{ :title => "Test" }] }
+          # becomes... [[{:title => "Test", :director_name => "A"}], ...[], ... []]
+          results = movies_with_directors_set(test_data)
+          expect(results.first.first[:director_name]).to eq("Byron Poodle"),
+            "The first element of the AoA should have 'Byron Poodle' as :director_name"
+        end
 
-#         it 'correctly "distributes" Nancy Drew as :director_name of the last film' do
-#           results = movies_with_directors_set(test_data)
-#           expect(results.last.first[:director_name]).to eq("Nancy Drew"),
-#             "The last element of the AoA should have 'Nancy Drew' as :director_name"
-#         end
-#       end
-#    end
-#  end
-#end
+        it 'correctly "distributes" Nancy Drew as :director_name of the last film' do
+          results = movies_with_directors_set(test_data)
+          expect(results.last.first[:director_name]).to eq("Nancy Drew"),
+            "The last element of the AoA should have 'Nancy Drew' as :director_name"
+        end
+      end
+    end
+  end
+end
 
 describe 'gross_per_studio' do
   it 'collects each movie based on the studio key' do
